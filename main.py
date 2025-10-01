b029564de3221c1bbe8515c297c9e8b36aa20058
do05267794-ship-it <do05267794@priestley.ac.uk>
2025-10-01 10:24:05 +0100

    Add files via upload

---
+++ main.py
@@ -0,0 +1,24 @@
+# Program name
+# Dan Olateju
+# 01/10/25
+# OCR AS Level Computer Science
+
+def three():
+  pass
+
+def two():
+  pass
+
+def one():
+  pass
+
+def main():
+  pass
+
+
+
+
+
+
+if (__name__ == "__main__"):
+    main()
